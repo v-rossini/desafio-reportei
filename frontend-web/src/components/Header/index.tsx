ec18519450345af40e2c6cbfbc80a09fa75ba1a2
Vitor Rossini <rossinivitor@hotmail.com>
2021-08-12 02:16:13 -0300

    header criado

---
+++ frontend-web/src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './styles.css'
+
+const Header = () => (
+    <header className = 'main-header'>
+        <img src = "https://img-premium.flaticon.com/png/512/2363/premium/2363876.png?token=exp=1628744949~hmac=8feb910ac72828f3a75b2d4ecc7bc6a6" alt = "icone" />
+        <div className = "logo-text">
+            <span className="logo-text-1">Stat Wars</span>
+        </div>
+            
+    </header>
+);
+
+export default Header;
