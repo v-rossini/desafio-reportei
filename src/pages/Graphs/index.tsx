a07215846217fb9f4463426f8f95f5e7f6dca659
Vitor Rossini <rossinivitor@hotmail.com>
2021-08-16 12:04:23 -0300

    corrigindo a estrutura do projeto

--- src/pages/Graphs/index.tsx
+++ src/pages/Graphs/index.tsx
@@ -30,29 +30,24 @@ async function getApiData() {
     let pageCounts = {}
 
     
-//        console.log("buscando valores")
+
     await Promise.all( Object.keys(pages).map(async (key) => {
         await axios.get(`${BASE_URL}/${key}`).then(response => {
-//                    console.log(`dados de ${key} obtidos`)
-//                    console.log(response.data.results)
+
                 data[key] = response.data.results;
                 pageCounts[key] = Math.ceil(response.data.count / 10)
-//                    console.log(`dados de ${key} processados`)
+
         }).then(async () => {
             for (var i = 2; i<=pageCounts[key]; i++) {
-//                    console.log(`Requisitando p.${i} de ${key}`)
+
                await  axios.get(`${BASE_URL}/${key}/?page=${i}`).then(response => {
                     data[key] = data[key].concat(response.data.results)}).catch(err => console.log(err))
-//               console.log(`p.${i} de ${key} processada`)
-//                    console.log(data)
+
             }
         })
     }));
 
     
-
-
-//   console.log(data)
    return data
 
 } 
@@ -77,50 +72,57 @@ const Graphs = () => {
         <Filter link="/Data"  text="VER TABELAS" search = {true}  />
         <div className="chart-container">
             <div className="top-related">
-                <h1 className="top-related-title">Quantidades de registros:</h1>
-                <div className = "games-container">
+                
+                <div className = "records-container">
                     {
-                    stateData ? (
+                    
+                    stateData ? (<div>
+                    <h1 className="top-related-title">Quantidades de registros:</h1>    
                     <Chart 
                     options = {barOptions}
                     type = "bar"
                     height = "200"
                     width = "900"
                     series = {[{ data: getQuantity(stateData)}]}/>
-
+                    </div>
                     ) : (<div><h1>Carregando dados.</h1>
                         <h2>Se isso demorar, é possível que a API esteja apresentando alguma lentidão.</h2>
                         <h2>Você pode verificar o progresso das requisições na sessão "rede" das ferramentas de desenvolvedor do navegador.</h2></div>)  
                     }                
 
                 </div>
-                <h1 className="top-related-title">Maiores planetas (diâmetro)</h1>
-                <div className = "games-container">
+
+                <div className = "records-container">
                     {
+                    
                     stateData ? (
+                    <div>                
+                    <h1 className="top-related-title">Maiores planetas (diâmetro)</h1>
                     <Chart 
                     options = {barOptions}
                     type = "bar"
                     height = "200"
                     width = "900"
                     series = {[{ data: biggestXinY(stateData, "planets", "diameter", 5 )}]}/>
-
+                    </div>
                     ) : (<h1></h1>)  
                     }
                                     
 
                 </div>
-                <h1 className="top-related-title">Maiores planetas (população)</h1>
-                <div className = "games-container">
+
+                <div className = "records-container">
                     {
                     stateData ? (
+                        <div>
+                                            <h1 className="top-related-title">Maiores planetas (população)</h1>
                     <Chart 
                     options = {barOptions}
                     type = "bar"
                     height = "200"
                     width = "900"
                     series = {[{ data: biggestXinY(stateData, "planets", "population", 5 )}]}/>
-
+                    </div>
                     ) : (<h1></h1>)  
                     }
                                     
@@ -129,58 +131,70 @@ const Graphs = () => {
             </div>
             <div className="charts">
                 <div className="platform-chart">
-                    <h2 className="top-related-title">5 Personagens com mais veículos</h2>
-                    <div className = "games-container">
+
+                    <div className = "records-container">
                         {
                         stateData ? (
+                            <div>
+                             <h2 className="top-related-title">5 Personagens com mais veículos</h2>
                         <Chart 
                         options = {{ ...pieOptions , labels: MostXinY_Names(stateData, "people", "vehicles", 5)}}
                         type = "donut"
                         series = {MostXinY_Values(stateData, "people", "vehicles", 5)}
                         width = "350"
-                        />) : (<div></ div>)
+                        />
+                        </div>
+                        ) : (<div></ div>)
                         }
                     </div>
                 </div>
                 <div className="platform-chart">
-                    <h2 className="top-related-title">5 personagens com mais naves</h2>
-                    <div className = "games-container">
+                    <div className = "records-container">
                         {
                         stateData ? (
+                            <div>
+                             <h2 className="top-related-title">5 personagens com mais naves</h2>
                         <Chart 
                         options = {{ ...pieOptions , labels: MostXinY_Names(stateData, "people", "starships", 5)}}
                         type = "donut"
                         series = {MostXinY_Values(stateData, "people", "starships", 5)}
                         width = "350"
-                        />) : (<div></ div>)
+                        />
+                        </div>
+                        ) : (<div></ div>)
                         }
                     </div>
                 </div>
                 <div className="platform-chart">
-                    <h2 className="top-related-title">Personagens em mais filmes</h2>
-                    <div className = "games-container">
+
+                    <div className = "records-container">
                         {
                         stateData ? (
+                        <div>
+                        <h2 className="top-related-title">Personagens em mais filmes</h2>
                         <Chart 
                         options = {{ ...pieOptions , labels: MostXinY_Names(stateData, "people", "films", 5)}}
                         type = "donut"
                         series = {MostXinY_Values(stateData, "people", "films", 5)}
                         width = "350"
-                        />) : (<div></ div>)
+                        />
+                        </div>) : (<div></ div>)
                         }
                     </div>
                 </div>
                 <div className="platform-chart">
-                    <h2 className="top-related-title">Planetas com mais personagens</h2>
-                    <div className = "games-container">
+                    <div className = "records-container">
                         {
                         stateData ? (
+                            <div>
+                                 <h2 className="top-related-title">Planetas com mais personagens</h2>
                         <Chart 
                         options = {{ ...pieOptions , labels: MostXinY_Names(stateData, "planets", "residents", 5)}}
                         type = "donut"
                         series = {MostXinY_Values(stateData, "planets", "residents", 5)}
                         width = "350"
-                        />) : (<div></ div>)
+                        />
+                        </div>) : (<div></ div>)
                         }
                     </div>
                 </div>
